3733c12ea621fda0f99bcdc808ca68f4a3919cb9
LaureanoNatalutti <79203128+LaureaDev@users.noreply.github.com>
2024-02-12 15:55:04 -0300

    translation

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,25 @@ var typed = new Typed(".text", {
     backDelay:1000,
     loop:true
 });
+
+
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+
+const changeLanguage = async language => {
+    const requestJson = await fetch(`/languages/${language}.json`)
+    const texts = await requestJson.json()
+
+  for ( const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+  }
+}
+
+flagsElement.addEventListener('click', (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+})
